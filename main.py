0f83d2f7f47d85496d8cc6451c71acb0f11ea8e8
ahmedabdelrahmaan <75083450+ahmedabdelrahmaan@users.noreply.github.com>
2023-06-29 14:01:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if uploaded_file is not None:
 
     fig.update_layout(hovermode="x")
     fig.update_layout(hovermode="x unified")
-    fig.update_traces(hovertemplate = '<i>y_AXIS1</i>: $%{y:.2f}'+
+    fig.update_traces(hovertemplate = '<br>'+
                                         '<i>%{text}</i>',
                       text = ['Your extra variable {}'.format(i) for i in dataframe['comment']])
     st.plotly_chart(fig)
